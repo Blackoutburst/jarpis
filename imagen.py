c814fa2e38d60aa1c7591da9e218462d48244845
Florian Chanson <fchanson@solem.fr>
2025-06-26 17:44:47 +0200

    added image gen

--- imagen.py
+++ imagen.py
@@ -1,18 +1,35 @@
-from diffusers import AutoPipelineForText2Image
 import torch
 from datetime import datetime
 from PIL import Image
+from diffusers import StableDiffusionXLPipeline
 
-pipe = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16")
+pipe = StableDiffusionXLPipeline.from_pretrained(
+    "cagliostrolab/animagine-xl-4.0",
+    torch_dtype=torch.float16,
+    use_safetensors=True,
+    custom_pipeline="lpw_stable_diffusion_xl",
+    add_watermarker=False
+)
 pipe.to("mps")
 
-def generate_image(prompt):
-    global pipe
+negative_prompt = "lowres, bad anatomy, bad hands, text, error, missing finger, extra digits, fewer digits, cropped, worst quality, low quality, low score, bad score, average score, signature, watermark, username, blurry"
 
-    image = pipe(prompt=prompt, num_inference_steps=1, guidance_scale=0.0).images[0]
+def generate_image(prompt):
+    global base
+    global refiner
+    global negative_prompt
+
+    image = pipe(
+        prompt = prompt + ", masterpiece, high score, great score, absurdres",
+        negative_prompt=negative_prompt,
+        width=832,
+        height=1216,
+        guidance_scale=5,
+        num_inference_steps=28
+    ).images[0]
+    
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     filename = f"image_{timestamp}.png"
     image.save(filename)
     
     return filename
-
